8a5c4ba55bd7027881c4d6c0ccb2a18f6fa75b0e
Skye Slattery <dss354@cornell.edu>
2024-03-25 17:51:29 -0400

    Add ocaml-canvas library and made a white screen and exit message

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,15 @@
-let () = print_endline "Hello, World!"
+open OcamlCanvas.V1
+
+let width = 1000
+let height = 400
+
+let () = 
+  Backend.init ();
+
+  let c = Canvas.createOnscreen ~title:"CamelGO"
+            ~pos:(300, 200) ~size:(width, height) () in
+  
+  Canvas.show c;
+
+  Backend.run (fun () ->
+    Printf.printf "Goodbye !\n")
