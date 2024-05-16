58ee8282bd3fe8f0260f5c5e851873c92c764e6b
cdagins <163474923+cdagins@users.noreply.github.com>
2024-05-15 23:43:58 -0400

    Most comments in menu.mli
    - need to add one for retain_event

--- lib/menu.mli
+++ lib/menu.mli
@@ -1,15 +1,26 @@
+(* List of events to process in the game. *)
 val events : unit React.event list ref
+
+(* TBA *)
 val retain_event : unit React.event -> unit
+
+(* Gets rid of all active events. *)
 val clear_events : unit -> unit
 
+(* State of the background. *)
 type bg_state = { mutable image_opt : OcamlCanvas.V1.Canvas.t option }
 
+(* Value representing the active background. *)
 val bg_img : bg_state
 
+(* The type of the state of the menu. *)
 type menu_state = {
   mutable best_score : int;
   mutable is_active : bool;
 }
 
+(* Initializes the menu state. *)
 val initialize_menu_state : int -> menu_state
+
+(* Creates the menu itself. *)
 val start_menu : int -> (int -> unit) -> 'a
