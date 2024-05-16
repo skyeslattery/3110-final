7b9eeae10d2168fbe3883fa002c5beb3b22a748a
cdagins <163474923+cdagins@users.noreply.github.com>
2024-05-15 23:56:01 -0400

    Comments for decorations.mli

--- lib/decorations.mli
+++ lib/decorations.mli
@@ -1,5 +1,4 @@
-(**The type of a decoration representation whose elements are type
-   decoration_type*)
+(* The type of a decoration object's style (not to be confused with the object itself). *)
 type decoration_type =
   | Star1
   | Star2
@@ -11,23 +10,42 @@ type decoration_type =
   | Bump1
   | Bump2
 
+(* The type of a decoration object. *)
 type t = {
   mutable pos : float * float;
   vel : float * float;
   style : decoration_type;
 }
-(**The type of a decoration whose elements are type t*)
 
+(* Increments an input position with screen scrolling. *)
 val adjust_position : 'a -> float -> 'a * float
-(**Adjust position x y adjusts the *)
 
+(* Creates a star decoration with a particular position and velocity. *)
 val create_star : float -> float -> float -> float -> t
+
+(* Creates a cloud decoration with a particular position and velocity. *)
 val create_cloud : float -> float -> float -> float -> t
+
+(* Creates a grass decoration with a particular position and velocity. *)
 val create_grass : float -> float -> float -> float -> t
+
+(* Updates the position of a decoration. *)
 val update_dec : t -> float -> float -> t
+
+(* Creates a bump on the ground with a particular position and velocity. *)
 val create_bump : float -> float -> float -> float -> t
+
+(* Returns the x-component of a decoration's position. *)
 val get_dec_x : t -> float
+
+(* Returns the y-component of a decoration's position. *)
 val get_dec_y : t -> float
+
+(* Returns a tuple [(x, y)] representing a decoration's position. *)
 val get_dec_pos : t -> float * float
+
+(* Returns a tuple [(x, y)] representing a decoration's velocity. *)
 val get_dec_vel : t -> float * float
+
+(* Returns the type of a particular decoration (star, cloud, etc.) as an int. *)
 val get_dec_type : t -> int
